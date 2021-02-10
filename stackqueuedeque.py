aeabaa08cd7c73a1193450ca74c332b8539f740b
vivalavita555 <brandon.robson2000@gmail.com>
2021-02-10 20:05:55 +0000

    Created stack, queue and deque classes

---
+++ stackqueuedeque.py
@@ -0,0 +1,58 @@
+class Stack(object):
+
+    def __init__(self):
+        self.items = []
+    
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        return self.items[len(self.items) - 1]
+    
+    def size(self):
+        return len(self.items)
+
+class Queue(object):
+    
+    def __init__(self):
+        self.items[]
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self,item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+    
+    def size(self):
+        return len(self.items)
+
+class Deque(object):
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def addFront(self,item):
+        self.items.append(item)
+    
+    def addRear(self,item):
+        self.items.insert(0,item)
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
